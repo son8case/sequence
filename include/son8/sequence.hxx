3115a2330f49a9e50c7cb1f148b739cc90f386bc
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-02-09 13:31:56 +0200

    binary search lower upper and equal, not found improvement

--- include/son8/sequence.hxx
+++ include/son8/sequence.hxx
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <cstdint>
 
+#define SON8_SEQUENCE_NOT_FOUND( Type, seq ) Type{ seq.end( ), seq.end( ) }
+
 namespace son8::sequence {
     // aliases
     using Size = std::size_t;
@@ -34,6 +36,16 @@ namespace son8::sequence {
         return seq.end( ) - seq.beg( );
     }
     template< typename Type >
+    bool is_valid( Type seq ) noexcept {
+        static_assert( Type::category( ) == Category::Adjacent );
+        return seq.beg( ) < seq.end() ;
+    }
+    template< typename Type >
+    bool is_empty( Type seq ) noexcept {
+        static_assert( Type::category( ) == Category::Adjacent );
+        return read_diff( seq ) <= 0;
+    }
+    template< typename Type >
     auto read_middle( Type seq ) noexcept {
         static_assert( Type::category( ) == Category::Adjacent );
         return seq.beg( ) + ( read_diff( seq ) >> 1u );
@@ -53,6 +65,11 @@ namespace son8::sequence {
     void edit_beg( Out< Type > edit, Ptr< typename Type::Data > value ) noexcept { Edit< Type >::beg( edit, value ); }
     template< typename Type >
     void edit_end( Out< Type > edit, Ptr< typename Type::Data > value ) noexcept { Edit< Type >::end( edit, value ); }
+    // not found
+    template< typename Type >
+    auto not_found( Type seq ) noexcept -> Type {
+        return Type{ seq.end( ), seq.end( ) };
+    }
     // Adjacent category requires contiguous containters
     template< typename Type >
     class Adjacent final {
@@ -87,14 +104,51 @@ namespace son8::sequence {
     auto sorted_match( Type seq, Ref< Data > data ) -> Type {
         static_assert( Type::category( ) == Category::Adjacent
             , "son8::sequence sorted_match requires sequence to be adjacent (contigues)" );
-        auto notFound = Type{ seq.end( ), seq.end( ) };
-        while ( read_diff( seq ) ) {
+        while ( is_valid( seq ) ) {
             auto mid = read_middle( seq );
             if/*_*/ ( *mid < data ) edit_beg( seq, mid + 1 );
             else if ( data < *mid ) edit_end( seq, mid );
             else return Type{ mid, mid + 1 };
         }
-        return notFound;
+        return not_found( seq );
+    }
+    // -- binary search bound lower
+    template< typename Type, typename Data = typename Type::Data >
+    auto sorted_lower( Type seq, Ref< Data > data ) -> Type {
+        static_assert( Type::category( ) == Category::Adjacent
+            , "son8::sequence sorted_lower requires sequence to be adjacent (contigues)" );
+        auto end = seq.end( );
+        while ( is_valid( seq ) ) {
+            if ( auto mid = read_middle( seq ); *mid < data ) edit_beg( seq, mid + 1 );
+            else edit_end( seq, mid );
+        }
+        bool found = seq.end( ) != end && *seq.end( ) == data;
+        return found ? Type{ seq.end( ), seq.end( ) + 1 } : not_found( seq );
+    }
+    // -- binary search bound upper
+    template< typename Type, typename Data = typename Type::Data >
+    auto sorted_upper( Type seq, Ref< Data > data ) -> Type {
+        static_assert( Type::category( ) == Category::Adjacent
+            , "son8::sequence sorted_upper requires sequence to be adjacent (contigues)" );
+        auto beg = seq.beg( );
+        while ( is_valid( seq ) ) {
+            auto mid = read_middle( seq );
+            if ( data < *mid ) edit_end( seq, mid );
+            else edit_beg( seq, mid + 1 );
+        }
+        bool found = seq.beg( ) != beg && *( seq.end( ) - 1 ) == data;
+        return found ? Type( seq.end( ) - 1, seq.end( ) ) : not_found( seq );
+    }
+    // -- binary search range equal
+    template< typename Type, typename Data = typename Type::Data >
+    auto sorted_equal( Type seq, Ref< Data > data ) -> Type {
+        static_assert( Type::category( ) == Category::Adjacent
+            , "son8::sequence sorted_equal requires sequence to be adjacent (contigues)" );
+        auto match = sorted_match( seq, data );
+        if ( is_empty( seq ) ) return not_found( seq );
+        auto lower = sorted_lower( Type{ seq.beg( ), match.end( ) }, data );
+        auto upper = sorted_upper( Type{ match.end( ), seq.end( ) }, data );
+        return Type{ lower.beg( ), upper.end( ) };
     }
 }
 
